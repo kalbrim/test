2deca6496622fac7f3ce9bef87b36fd63f7e465f
Brendan Comiskey <comiskeybrendan@gmail.com>
2021-10-11 13:14:09 -0400

    Big O

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -6,6 +6,28 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("This is a project test.");
 		System.out.println("This is an update.");
+		//
+		// 0(1) constant
+		System.out.println("This is constant notation");
+		
+		// O(n) linear growth with input
+		int[] arr = {1, 2, 3, 4, 5, 6, 7};
+		for (int i = 0; i < arr.length; i++) {
+			// syso is still o(1) constant runtime
+			System.out.println(arr[i]);
+		}
+		int count = 0;
+		// O(n^2) 
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println("outer loop at: " + i);
+			for (int j = 0; j < arr.length; j++) {
+				System.out.println("inner loop at: " + j);
+				if (arr[i] != arr[j]) {
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
 
 	}
 
